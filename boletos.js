4a35edb20b3f12ac13b43300d8b9f8de47cfe3fd
Juan Bazan <juan.bazan88@yahoo.com>
2024-10-31 11:12:42 -0300

    Micro desafío 1

---
+++ boletos.js
@@ -0,0 +1,20 @@
+let nombre = "Braian";
+let apellido = "Alonso";
+let edad = 34;
+let telefono = "0123456789";
+let socio = true;
+let fechaPartido = "2024-11-12";
+let horaPartido = "19:00";
+let numeroSilla = 13;
+let precioBoleto = 310.50;
+
+
+console.log(`Nombre: ${nombre}, Tipo: ${typeof nombre}`);
+console.log(`Apellido: ${apellido}, Tipo: ${typeof apellido}`);
+console.log(`Edad: ${edad}, Tipo: ${typeof edad}`);
+console.log(`Número de Teléfono: ${telefono}, Tipo: ${typeof telefono}`);
+console.log(`Socio: ${socio}, Tipo: ${typeof socio}`);
+console.log(`Fecha del Partido: ${fechaPartido}, Tipo: ${typeof fechaPartido}`);
+console.log(`Hora del Partido: ${horaPartido}, Tipo: ${typeof horaPartido}`);
+console.log(`Número de Silla: ${numeroSilla}, Tipo: ${typeof numeroSilla}`);
+console.log(`Precio del Boleto: ${precioBoleto}, Tipo: ${typeof precioBoleto}`);
