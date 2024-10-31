bf096996c7a70e451a25305066b1449181431851
Juan Bazan <juan.bazan88@yahoo.com>
2024-10-31 11:34:01 -0300

    Micro desafío extra

---
+++ operaciones.js
@@ -0,0 +1,19 @@
+let num1 = 24;
+let num2 = 8;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+let modulo = num1 % num2;
+
+let mayor = num1 > num2;
+let menor = num1 < num2;
+
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicación:", multiplicacion);
+console.log("División:", division);
+console.log("Módulo:", modulo);
+console.log("¿Es el número 1 mayor que el número 2?", mayor);
+console.log("¿Es el número 1 menor que el número 2?", menor);
